b9a1dc14fb00e997232ee9b9b471af7a4ac0a5a6
jlownnjfln <128469674+jlownnjfln@users.noreply.github.com>
2023-05-03 09:38:39 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 # 设置各变量
 WSPATH=${WSPATH:-'argo'}
-UUID=${UUID:-'c664751b-b662-48f9-b2aa-2f0d4d337a7f'}
+UUID=${UUID:-'e70401e8-fc2c-44c8-af43-4480cdf84387'}
 
 # 哪吒三个参数，不需要的话可以留空，删除或在这三行最前面加 # 以注释
 NEZHA_SERVER= 
@@ -10,8 +10,8 @@ NEZHA_PORT=
 NEZHA_KEY=
 
 # Argo 固定域名隧道的两个参数,这个可以填 Json 内容或 Token 内容，获取方式看 https://github.com/fscarmen2/X-for-Glitch，不需要的话可以留空，删除或在这三行最前面加 # 以注释
-ARGO_AUTH=''
-ARGO_DOMAIN=
+ARGO_AUTH='{"AccountTag":"2c029e9aa5a913046aac6e5634f47f60","TunnelSecret":"jcDXvcTR36S8oCUcCx5LYHD6JKqDpNew+n/ms7zeX70=","TunnelID":"e2423844-f7e8-40bb-881a-8dd1af66bf85"}'
+ARGO_DOMAIN=back1.breakee.ml
 
 generate_config() {
   cat > config.json << EOF
